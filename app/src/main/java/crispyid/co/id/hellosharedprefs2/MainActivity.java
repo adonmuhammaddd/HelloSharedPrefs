a069acb238e5996ee115d76d1efbc788097fb338
bangmadown <bang.madown@gmail.com>
2018-11-15 10:05:07 +0700

    first commt

---
+++ app/src/main/java/crispyid/co/id/hellosharedprefs2/MainActivity.java
@@ -0,0 +1,79 @@
+package crispyid.co.id.hellosharedprefs2;
+
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    // Current count
+    private int mCount = 0;
+    // Current background color
+    private int mColor;
+    // Text view to display both count and color
+    private TextView mShowCountTextView;
+
+    // Key for current count
+    private final String COUNT_KEY = "count";
+    // Key for current color
+    private final String COLOR_KEY = "color";
+
+    private SharedPreferences mPreferences;
+    private String sharedPrefFile = "crispyid.co.id.hellosharedprefs2";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize views, color
+        mShowCountTextView = findViewById(R.id.count_textview);
+        mColor = ContextCompat.getColor(this,
+                R.color.default_background);
+
+        mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+
+        // Restore Preferences
+        mCount = mPreferences.getInt(COUNT_KEY, 0);
+        mShowCountTextView.setText(String.format("%s", mCount));
+        mColor = mPreferences.getInt(COLOR_KEY, mColor);
+        mShowCountTextView.setBackgroundColor(mColor);
+    }
+    public void changeBackground(View view) {
+        int color = ((ColorDrawable) view.getBackground()).getColor();
+        mShowCountTextView.setBackgroundColor(color);
+        mColor = color;
+    }
+    public void countUp(View view) {
+        mCount++;
+        mShowCountTextView.setText(String.format("%s", mCount));
+    }
+    public void reset(View view) {
+        // Reset count
+        mCount = 0;
+        mShowCountTextView.setText(String.format("%s", mCount));
+
+        // Reset color
+        mColor = ContextCompat.getColor(this,
+                R.color.default_background);
+        mShowCountTextView.setBackgroundColor(mColor);
+
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        preferencesEditor.clear();
+        preferencesEditor.apply();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        preferencesEditor.putInt(COUNT_KEY, mCount);
+        preferencesEditor.putInt(COLOR_KEY, mColor);
+        preferencesEditor.apply();
+    }
+}
